f6e6b7941ce802daab4aa1d218e5e4ddee00f969
thamidurm <thamidurm@gmail.com>
2020-06-04 12:45:30 +0530

    Refactor serializer.py

--- app/serializers.py
+++ app/serializers.py
@@ -1,22 +1,24 @@
 from rest_framework import serializers
-from rest_framework_mongoengine import serializers as mongo_serializers
-from .validators import ValidateBookOwnership
+from .validators import ValidateBookOwnership, required
 from .models import Content, Book
-from creators.serializers import ShallowCreatorSerializer
+from creators.models import Creator
+from creators.serializers import ShallowCreatorSerializer, SubCreatorSerializer
 from rest_framework.validators import UniqueValidator
 from django.http import Http404
 
+from .fields import *
 
-class BookSerializer(mongo_serializers.DocumentSerializer):
-    id = serializers.CharField(read_only=True, required=False)
-    publisher = ShallowCreatorSerializer()
 
-    class Meta:
-        model = Book
-        fields = ['id', 'covers','title', 'isbns', 'authors', 'active', 'publisher']
-        read_only_fields = ['publisher', 'active']
-        depth=2
-
+class BookSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
+    title = serializers.CharField(required=True)
+    isbns = serializers.ListField(child=ISBNField(), allow_empty=False)
+    authors = serializers.ListField(child=serializers.CharField(), allow_empty=False)
+    active = serializers.BooleanField(read_only=True)
+    publisher = CreatorShallowField(read_only=True)
+    covers = serializers.ListField(child=serializers.URLField(), allow_empty=False)
+    active = serializers.BooleanField(required=False)
+    
     def create(self, validated_data):
         title = validated_data['title']
         isbns = validated_data['isbns']
@@ -31,67 +33,78 @@ class BookSerializer(mongo_serializers.DocumentSerializer):
             covers=covers,
             publisher=publisher
         )
-      
         return instance
 
-
-class ContentSerializer(mongo_serializers.DocumentSerializer):
-    id = serializers.CharField(read_only=True, 
-        required=False, 
-        validators=[
-            UniqueValidator(queryset=Book.objects.all())
-        ]
-    )
-
-    creator = ShallowCreatorSerializer()
-    book = BookSerializer()
-    class Meta:
-        model =  Content
-        fields = '__all__'
-        read_only_fields = ['creator', 'active']
-        validators= [ValidateBookOwnership()]
-        depth=2
-
-    def get_book_or_404(id):
+    def validate_publisher(self, value):
         try:
-            book = Book.objects.get(id=book_id)
-            return book
-        except Book.DoesNotExist:
-            raise Http404
+            publisher = Creator.objects.get(id=value)
+            return publisher
+        except Exception:
+            raise serializers.ValidationError('Invalid publisher id')
 
-    def update(self,instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.images = validated_data.get('images', instance.images)
-        instance.file = validated_data.get('file', instance.file)
-
-        if 'book' in validated_data.keys():
-            book_id = validated_data.get('book')
-            instance.book = get_book_or_404(book_id)
-        instance.save()
+    def update(self, instance, validated_data):
+        title = validated_data.get('title', instance.title)
+        isbns = validated_data.get('isbns', instance.isbns)
+        authors = validated_data.get('authors', instance.authors)
+        covers = validated_data.get('covers', instance.covers)
         return instance
-        
+
+    
+class ContentSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
+    title = serializers.CharField(required=True)
+    description = serializers.CharField(required=True)
+    images = serializers.ListField(child=serializers.URLField(), allow_empty=False)
+    file = serializers.URLField(required=True)
+    book = BookField(required=True)
+    creator = CreatorShallowField(required=False)
+    active = serializers.BooleanField(required=False)
+    animated = serializers.BooleanField(required=True)
+    size = serializers.IntegerField(required=True)
+
+    class Meta:
+        validators = [ValidateBookOwnership()]
     def create(self, validated_data):
         title = validated_data['title']
         description = validated_data['description']
         images = validated_data['images']
         creator = validated_data['creator']
         file = validated_data['file']
-        book = get_book_or_404(validated_data['book']);
+        book = validated_data['book']
+        size = validated_data['size']
+        animated = validated_data['animated']
         instance = Content.objects.create(
             title=title,
             description=description,
             images=images,
             book=book,
             file=file,
-            creator=creator
+            creator=creator,
+            size=size,
+            animated=animated
         )
         return instance
 
-class ContentShallowSerializer(ContentSerializer):
-    class Meta:
-        model =  Content
-        fields = ['id', 'title', 'description', 'images', 'file', 'size']
+    def update(self,instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.images = validated_data.get('images', instance.images)
+        instance.file = validated_data.get('file', instance.file)
+        instance.book = validated_data.get('book', instance.book)
+        instance.animated = validated_data.get('animated', instance.animated)
+        instance.size = validated_data.get('size', instance.size);
+        instance.save()
+        return instance
+
+class ContentShallowSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    images = serializers.ListField(child=serializers.URLField())
+    file = serializers.URLField()
+    active = serializers.BooleanField()
+    animated = serializers.BooleanField()
+    size = serializers.IntegerField()
 
 
 class BookDeepSerializer(BookSerializer):
